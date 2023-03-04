b41eedd973efdfa1b974dda6c14be215d043406f
Or Mizrahi <or0mizrahi@gmail.com>
2023-03-04 23:07:13 +0200

    creating a categories page

--- lib/categories_page.dart
+++ lib/categories_page.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+import './category_item.dart';
+
 class CategoriesPage extends StatelessWidget {
   const CategoriesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Meals'),
       ),
-      children: [],
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((categoryData) => CategoryItem(
+                  categoryData.title,
+                  categoryData.color,
+                ))
+            .toList(),
+      ),
     );
   }
 }
