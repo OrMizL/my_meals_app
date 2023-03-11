90840dc9ca0cadc7df330f809820a12e091ac3b0
Or Mizrahi <or0mizrahi@gmail.com>
2023-03-11 22:37:36 +0200

    enhancing meal item card

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -17,6 +17,38 @@ class MealItem extends StatelessWidget {
     @required this.affordability,
   });
 
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Beginner:
+        return 'Beginner';
+        break;
+      case Complexity.Intermediate:
+        return 'Intermediate';
+        break;
+      case Complexity.Expert:
+        return 'Expert';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Cheap:
+        return 'Cheap';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      case Affordability.Luxurious:
+        return 'Luxurious';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
   void selectMeal() {}
 
   @override
@@ -44,9 +76,65 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
-                )
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.black54,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          blurRadius: 15,
+                          spreadRadius: 5,
+                        ),
+                      ],
+                    ),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
               ],
             ),
+            Padding(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.schedule),
+                        SizedBox(width: 8,),
+                        Text('$duration min'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.handyman),
+                        SizedBox(width: 8,),
+                        Text(complexityText),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.wallet),
+                        SizedBox(width: 8,),
+                        Text(affordabilityText),
+                      ],
+                    ),
+                  ],
+                ))
           ],
         ),
       ),
