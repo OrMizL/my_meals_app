4c1f5caff1d04c959e877aaaa66e59a7091a33c9
Or Mizrahi <or0mizrahi@gmail.com>
2023-04-12 21:18:49 +0300

    filters logic

--- lib/screens/filters_page.dart
+++ lib/screens/filters_page.dart
@@ -2,16 +2,101 @@ import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
-class FiltersPage extends StatelessWidget {
+class FiltersPage extends StatefulWidget {
   const FiltersPage({Key key}) : super(key: key);
+
+  @override
+  State<FiltersPage> createState() => _FiltersPageState();
+}
+
+class _FiltersPageState extends State<FiltersPage> {
+  var _isGlutenFree = false;
+  var _isVegetarian = false;
+  var _isVegan = false;
+  var _isLactoseFree = false;
+
+  Widget _buildSwitchListTile(
+      String title, String subtitle, bool currentValue, Function updateValue) {
+    return SwitchListTile(
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.titleSmall,
+      ),
+      subtitle: Text(subtitle),
+      value: currentValue,
+      onChanged: updateValue,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Filters Page')),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Filters!'),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Filter out recipes',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _buildSwitchListTile(
+                  'Gluten Free',
+                  'Show only gluten free recipes',
+                  _isGlutenFree,
+                  (newValue) {
+                    setState(
+                      () {
+                        _isGlutenFree = newValue;
+                      },
+                    );
+                  },
+                ),
+                _buildSwitchListTile(
+                  'Vegetarian',
+                  'Show only vegetarian recipes',
+                  _isVegetarian,
+                  (newValue) {
+                    setState(
+                      () {
+                        _isVegetarian = newValue;
+                      },
+                    );
+                  },
+                ),
+                _buildSwitchListTile(
+                  'Vegan',
+                  'Show only vegan recipes',
+                  _isVegan,
+                  (newValue) {
+                    setState(
+                      () {
+                        _isVegan = newValue;
+                      },
+                    );
+                  },
+                ),
+                _buildSwitchListTile(
+                  'Lactose Free',
+                  'Show only lactose free recipes',
+                  _isLactoseFree,
+                  (newValue) {
+                    setState(
+                      () {
+                        _isLactoseFree = newValue;
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
